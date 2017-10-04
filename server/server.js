608ac564fd974382a4498ba78f39293275a4565a
Ted Shin <tdyshin@gmail.com>
2017-10-04 19:57:36 -0400

    Got python working with node

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const PythonShell = require('python-shell');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+// app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 // Answer API requests.
 app.get('/api', function (req, res) {
@@ -13,10 +14,30 @@ app.get('/api', function (req, res) {
   res.send('{"message":"Hello from the custom server!"}');
 });
 
-// All remaining requests return the React app, so it can handle routing.
-app.get('*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+app.get('/python', function (req, res) {
+
+  const options = {
+    scriptPath: path.resolve(__dirname, '../python'),
+  };
+  var pyshell = new PythonShell('assignment.py', options);
+  pyshell.on('message', function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    console.log(message);
+    res.send(message);
+  });
+
+  // end the input stream and allow the process to exit
+  pyshell.end(function (err) {
+    if (err) throw err;
+    console.log('finished');
+  });
+  res.send('');
 });
+
+// All remaining requests return the React app, so it can handle routing.
+// app.get('*', function(request, response) {
+//   response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+// });
 
 app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
