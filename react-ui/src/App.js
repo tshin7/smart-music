a43f8c721c5412772084e1e0abf2e52010d42ed4
Ted Shin <tdyshin@gmail.com>
2017-10-04 00:08:54 -0400

    Clean up create-react-app stuff

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -28,15 +27,8 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          {this.state.message}
-        </p>
+      <div>
+        <h1>Smart Music</h1>
       </div>
     );
   }
