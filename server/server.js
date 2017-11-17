26c8fae98209d94ab801c38712206f8d8c1a06bb
Ted Shin <tdyshin@gmail.com>
2017-11-16 21:26:50 -0500

    Update server.js so it works with compute engine

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const PythonShell = require('python-shell');
+const morgan = require('morgan'); // log requests to the console
+const bodyParser = require('body-parser'); // pull information from HTML POST
+var methodOverride = require('method-override'); // simulate DELETE and PUT
+var argv = require('optimist').argv;
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+// configuration
+mongoose.connect('mongodb://' + argv.be_ip + ':80/my_database');
+
+app.use(morgan('dev'); // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(methodOverride());
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
@@ -39,6 +51,6 @@ app.get('*', function(request, response) {
   response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
 });
 
-app.listen(PORT, function () {
+app.listen(PORT, argv.fe_ip, function () {
   console.log(`Listening on port ${PORT}`);
 });
